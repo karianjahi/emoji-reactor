0227ad10dfed3ab9748f219a1e6982b3a6b9049c
karianjahi <nkarianjahi@gmail.com>
2026-01-22 16:26:20 +0100

    Create a function that updates the display to increment emotion by 1

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const happyBtn = document.querySelector("#happy-btn");
 
+
+function updateCount(btnElement) {
+    let countEl = btnElement.querySelector(".count");
+    let countText = countEl.textContent;
+    let currCount = Number(countText.split("/")[0]);
+    if (currCount < 10)
+        countEl.textContent = `${(currCount + 1)}/10`;
+};
+
 happyBtn.addEventListener("click", () => {
-  let countEl = happyBtn.querySelector(".count");
-  let countText = countEl.textContent;
-  let currCount = Number(countText.split("/")[0]);
-  if (currCount < 10) {
-    countEl.textContent = `${(currCount + 1)}/10`;
-  }
-
+    updateCount(happyBtn);
 });
-
 
